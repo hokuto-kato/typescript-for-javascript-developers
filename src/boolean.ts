a3943e1705fd1f4961bfcc9f6c5f71dae510846d
hokuto-kato <hokuto15@gmail.com>
2024-04-30 01:40:33 +0900

    Add new boolean.ts module
    This commit introduces a new boolean.ts module with the boolean variable 'isFinished', showing the correct way to declare, initialize and modify boolean variables in TypeScript. Also included is an example of a type error that occurs when a boolean variable is assigned a value other than boolean.

---
+++ src/boolean.ts
@@ -0,0 +1,16 @@
+// "export {}"は、ファイルがスクリプトではなくモジュールであることを示すTypeScriptの記述。
+// この行がないと、TypeScriptはファイルをスクリプトとして扱い、
+// グローバルスコープで重複宣言を検出しているというエラーを投げてしまう。
+// 実際には何もエクスポートしない。
+export {}
+
+// boolean型の変数isFinishedを宣言し、初期値をtrueに設定
+let isFinished: boolean = true
+
+// isFinishedの値をfalseに更新しても方は合っているので問題ない
+isFinished = false
+
+// 数値をboolean型の変数に代入するとエラーになる
+// isFinished = 1
+
+console.log({ isFinished })
