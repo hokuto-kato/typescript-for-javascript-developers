d59a03abaebb90f61219c2b87e39df70426af6ba
hokuto-kato <hokuto15@gmail.com>
2024-04-30 02:07:57 +0900

    Add new file src/number-string.ts
    This commit adds a new file src/number-string.ts. In this file, the variables 'year' and 'name' are created and assigned a number and a string, respectively. The variable 'age' is also assigned a hexadecimal number.

---
+++ src/number-string.ts
@@ -0,0 +1,12 @@
+export {}
+
+let year:number = 1979
+// year = false
+console.log(year)
+
+let age:number = 0x2d // 16進数
+console.log(age)
+
+let name:string = "hokuto"
+// name = 10
+console.log(name)
