479b8f3aad8611955a92935f21d0df87b97b63bc
hokuto-kato <hokuto15@gmail.com>
2024-04-30 11:51:33 +0900

    Add tuple type definition in tuple.ts
    A new file named tuple.ts has been created to include a tuple type definition of profile. This allows constraining the order as `[string, number]`. Any change in this order, as illustrated in the code, will throw an error.

---
+++ src/tuple.ts
@@ -0,0 +1,9 @@
+export {}
+
+// profile:(string|number)[]と型推論されるが
+// 形の順序性に対する制約はもたせられない
+// その場合、tuple型を使う
+let profile: [string, number] = ['hokuto', 45]
+
+// 順番を変えるとエラー
+profile = [45, 'hokuto']
