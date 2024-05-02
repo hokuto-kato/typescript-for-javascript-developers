c9308b1baa1f977a9f90eb32f6332c95b6e41b80
hokuto-kato <hokuto15@gmail.com>
2024-05-02 13:46:48 +0900

    void関数の宣言と呼び出しを修正
    void型の関数returnNothingを宣言で、それを直接呼び出すように変更しました。これにより、未使用のconsole.logを削除しました。

--- src/void.ts
+++ src/void.ts
@@ -1,8 +1,10 @@
 export {}
 
 // 関数からのリターン値がない場合の型がvoid
-const returnNothing = (): void => {
+// undefined型でも間違いではないが、その場合はreturnを明示しないといけない
+// 関数に手を加えることは本筋ではないのでvoid形が推奨されている
+function returnNothing(): void {
   console.log("I don't return anything...")
 }
 
-console.log(returnNothing())
+returnNothing()
