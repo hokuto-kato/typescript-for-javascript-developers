6a471da236a20960ab229842abb0e0f708547a6b
hokuto-kato <hokuto15@gmail.com>
2024-05-02 23:47:01 +0900

    Personクラスの追加
    新たにPersonクラスを作成しました。このクラスにはnameとageのプロパティがあり、これらはconstructorで初期化します。また、profileメソッドを通じてプロフィール情報を文字列として取得できます。

---
+++ src/my-first-class.ts
@@ -0,0 +1,22 @@
+import { log } from 'console'
+
+export {}
+
+class Person {
+  // classの型定義はトップレベルで行う
+  name: string
+  age: number
+  // constructorで引数を受け取れる。
+  // ここで初期化処理を行う。
+  // constructorの戻り値の型定義はしない。
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+  profile(): string {
+    return `name:${this.name}, age:${this.age}`
+  }
+}
+// new演算子でインスタンス（実体）を作れる
+const taro = new Person('Taro', 30)
+console.log(taro.profile())
